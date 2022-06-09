c694b9714c1049a93196c3053e3278473b246af3
root <lionmonkeydesignapp@gmail.com>
2022-06-09 14:17:40 -0300

    projeto_soifer

---
+++ lib/Screens/Usuario_detalhes/index.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Usuario_ extends StatelessWidget {
+  const Usuario_({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('header sistema soifer'),
+      ),
+      body: Center(
+        child: Text("teste"),
+      ),
+    );
+  }
+}
